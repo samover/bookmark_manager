c831ab2bd6c4a9affd00111dedf56dcc8fc006d6
samover <samuel.overloop@gmail.com>
2015-11-03 15:21:20 +0000

    shows links in database on homepage

--- app/app.rb
+++ app/app.rb
@@ -3,11 +3,13 @@ require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    @link = Link.all(url: 'www.google.co.uk')
     erb :index
   end
 
-
+  get '/links' do
+    @link = Link.all
+    erb :links
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
