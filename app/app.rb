0b1984d5498109ddf6340cb8ba19806954ca2bc2
NULL-OPERATOR <x@cryptolab.net>
2015-11-05 12:14:36 +0000

    heoeeokorku

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,8 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-require './app/models/link'
+require './app/data_mapper_setup'
+
 
 class BookmarkManager < Sinatra::Base
   get '/' do
@@ -18,9 +19,15 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(title: params[:title], url: params[:url])
+    link = Link.create(title: params[:title], url: params[:url])
+    tag = Tag.create(name: params[:tag])
+    link.tags << tag
+    link.save
     redirect('/links')
   end
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,16 +1,9 @@
-require 'data_mapper'
-# require 'dm-postgres-adapter'
-
 class Link
   include DataMapper::Resource
 
   property :id, Serial
   property :title, String
   property :url, String
+
+  has n, :tags, :through => Resource
 end
-
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bmanager_#{ENV['RACK_ENV']}")
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
